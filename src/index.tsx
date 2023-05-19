ea4917939af4ae11aee56a16fd3cf35fd7951a72
ngocdai162 <89380180+ngocdai162@users.noreply.github.com>
2023-05-19 09:57:36 +0700

    update router for github page

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+/** @format */
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import "react-slideshow-image/dist/styles.css";
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
-import { BrowserRouter} from 'react-router-dom';
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+	document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-         <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<BrowserRouter>
+				<HashRouter basename='/'>
+					<App />
+				</HashRouter>
+			</BrowserRouter>
+		</Provider>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
